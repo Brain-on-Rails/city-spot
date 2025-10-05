c350216b80000a824ba7c88742cf7eeee643db5e
Greg M <git.15m27@passmail.net>
2025-10-05 17:22:49 +0200

    Setup frontend tests

---
+++ app/frontend/javascript/components/Map.svelte.spec.js
@@ -0,0 +1,6 @@
+
+import {expect, test } from 'vitest';
+
+test('Map', () => {
+  expect(1).toBe(1);
+})
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
@@ -19,4 +19,10 @@ export default defineConfig({
       svelte(),
       FullReload(['app/views/**/*', 'app/helpers/**/*', 'config/routes.rb'])
   ],
+    test: {
+      globals: true,
+        environment: 'jsdom',
+        setupFiles: './frontend/javascript/setupSpecs.js',
+        resolve: process.env.VITEST ? { conditions: ['browser'] } : undefined
+    }
 })
