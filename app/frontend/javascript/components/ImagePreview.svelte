e416ca95ce96873f4e7d85f9ebe7a988767069cf
Greg M <git.15m27@passmail.net>
2025-10-08 09:56:40 +0200

    Add interactive image preview

--- app/frontend/javascript/components/ImagePreview.svelte
+++ app/frontend/javascript/components/ImagePreview.svelte
@@ -1,40 +1,61 @@
 
 <script>
+    import PhotoSwipeLightbox from "photoswipe/lightbox";
+    import PhotoSwipe from "photoswipe";
     import {onMount, onDestroy} from 'svelte';
+    import 'photoswipe/style.css';
 
     let {
         input,
         previewContainer,
     } = $props();
 
-    let filesetInput = $state(null);
+    let images = $state([]);
+    let lightbox;
 
 
     onMount(() => {
         listenToInputChanges();
     })
 
+    function openLightbox(index) {
+        if (!images.length) return;
+        lightbox = new PhotoSwipeLightbox({
+            dataSource: images,
+            pswpModule: PhotoSwipe,
+        })
+        lightbox.init();
+        lightbox.loadAndOpen(index);
+    }
+
     function listenToInputChanges() {
         input.addEventListener('change', (event) => {
-            filesetInput = Array.from(event.target.files).map(file => ({
-                file,
-                name: file.name,
-                alt: file.name,
-                url: URL.createObjectURL(file)
-            }));
+            if (!event.target.files) return;
+            images = [];
+            const inputFiles = Array.from(event.target.files)
+            inputFiles.forEach(loadImage)
         })
+    }
+
+    function loadImage(inputFile) {
+        const src = URL.createObjectURL(inputFile);
+        const img = new Image();
+        img.onload = () => {
+            images.push({
+                src,
+                width: img.width,
+                height: img.height,
+                alt: inputFile.name,
+            })
+        }
+        img.src = src;
     }
 </script>
 
-
-{#if filesetInput}
-    <div class="flex gap-2 flex-wrap">
-    {#each filesetInput as file}
-        <div class="avatar">
-            <div class="w-32 rounded">
-                <img src={file.url} alt={file.alt} class="" />
-            </div>
-        </div>
+<div class="grid grid-cols-[repeat(auto-fit,minmax(100px,1fr))] gap-4 justify-start">
+    {#each images as image, i}
+        <button type="button" class="w-full h-full bg-gray-200" onclick={() => openLightbox(i)}>
+            <img src={image.src} alt={image.name} title={image.name} class="cursor-pointer w-full object-cover" />
+        </button>
     {/each}
-    </div>
-{/if}
+</div>
